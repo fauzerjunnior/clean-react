e91db4f134fd370998c5329a984094e9e6a613c3
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-03-24 21:11:35 -0300

    feat: add app router

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -7,38 +8,40 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      '@': path.join(__dirname, 'src'),
+    },
   },
-  module: { 
-    rules: [{
-      test: /\.ts(x?)$/, 
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, 
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'style-loader'
-        },
-        {
-          loader: 'css-loader',
-           options: {
-             modules: true
-           }
-        },
-        {
-          loader: 'sass-loader'
-        }
-      ]
-    }]
-  },  
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
   devServer: {
     static: './public',
     devMiddleware: {
@@ -48,9 +51,7 @@ module.exports = {
   },
   externals: {
     react: 'React',
-    "react-dom": 'ReactDOM'
+    'react-dom': 'ReactDOM',
   },
-  plugins: [
-    new CleanWebpackPlugin()
-  ]
-}
+  plugins: [new CleanWebpackPlugin()],
+};
