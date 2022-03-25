0c0d6184a23516fdb2e18b40912f10c99eedabea
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-03-24 21:45:11 -0300

    fix: change name sass loader

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,js,tsx,jsx}',
-    '!**/*.d.ts'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js,tsx,jsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'idenity-obj-proxy'
-  }
-}
+    '\\.scss$': 'identity-obj-proxy',
+  },
+};
