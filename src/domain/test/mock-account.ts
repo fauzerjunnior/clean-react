b1f805bec05fe3971c49aff2eb2a8dfc52371405
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-03-24 22:00:05 -0300

    refactor: change deprecated faker object

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,12 +1,12 @@
-import faker from "@faker-js/faker";
-import { AuthenticationParams } from "@/domain/usecases";
-import { AccountModel } from "@/domain/models";
+import faker from '@faker-js/faker';
+import { AuthenticationParams } from '@/domain/usecases';
+import { AccountModel } from '@/domain/models';
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
-  password: faker.internet.password()
+  password: faker.internet.password(),
 });
 
 export const mockAccountModel = (): AccountModel => ({
-  accessToken: faker.random.uuid()
-})
+  accessToken: faker.datatype.uuid(),
+});
--- src/infra/http/test/mock-axios.ts
+++ src/infra/http/test/mock-axios.ts
@@ -1,12 +1,12 @@
-import faker from "@faker-js/faker";
-import axios from "axios";
+import faker from '@faker-js/faker';
+import axios from 'axios';
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
   mockedAxios.post.mockResolvedValue({
     data: faker.random.word(),
-    status: faker.random.number()
+    status: faker.datatype.number(),
   });
 
   return mockedAxios;
-}
+};
