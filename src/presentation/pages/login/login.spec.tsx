6ee486555de3471ba4cc487114d55869ddbd4d0b
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-03-26 14:11:30 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Login from './login';
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+
+  return {
+    sut,
+  };
+};
 
 describe('Login component', () => {
   it('should not render spinner and error on start', () => {
-    const { getByTestId } = render(<Login />);
-    const errorWrap = getByTestId('error-wrap');
+    const { sut } = makeSut();
+    const errorWrap = sut.getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
   });
 
   it('should start with submit button disabled', () => {
-    const { getByTestId } = render(<Login />);
-    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const { sut } = makeSut();
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
 
   it('should have email status with required validation', () => {
-    const { getByTestId } = render(<Login />);
-    const emailStatus = getByTestId('email-status');
+    const { sut } = makeSut();
+    const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe('Campo obrigatório');
     expect(emailStatus.textContent).toBe('🔴');
   });
 
   it('should have password status with required validation', () => {
-    const { getByTestId } = render(<Login />);
-    const passwordStatus = getByTestId('password-status');
+    const { sut } = makeSut();
+    const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Campo obrigatório');
     expect(passwordStatus.textContent).toBe('🔴');
   });
