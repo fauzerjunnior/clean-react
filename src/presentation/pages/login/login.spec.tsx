a962e724e6f6d304a05da494f5b2bf1a7627f9f7
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-03-28 18:56:50 -0300

    test: ensure login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -64,4 +64,18 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
+
+  it('should show valid password state if validation succeds', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const passwordInput = sut.getByTestId('password');
+
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+    const passwordStatus = sut.getByTestId('password-status');
+
+    expect(passwordStatus.title).toBe('Sucesso!');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
