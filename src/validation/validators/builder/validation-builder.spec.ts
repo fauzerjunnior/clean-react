11d4c3d98163cd2c249217d795e4f7c050de4296
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-03 14:03:10 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -3,24 +3,41 @@ import {
   MinLengthValidation,
   RequiredFieldValidation
 } from '@/validation/validators';
+import faker from '@faker-js/faker';
 import { ValidationBuilder as sut } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build();
+    const field = faker.database.column();
+    const validations = sut.field(field).required().build();
 
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+    expect(validations).toEqual([new RequiredFieldValidation(field)]);
   });
 
   test('should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build();
+    const field = faker.database.column();
+    const validations = sut.field(field).email().build();
 
-    expect(validations).toEqual([new EmailValidation('any_field')]);
+    expect(validations).toEqual([new EmailValidation(field)]);
   });
 
   test('should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build();
+    const field = faker.database.column();
+    const length = faker.random.number();
+    const validations = sut.field(field).min(length).build();
 
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)]);
+    expect(validations).toEqual([new MinLengthValidation(field, length)]);
+  });
+
+  test('should return a list of validations', () => {
+    const field = faker.database.column();
+    const length = faker.random.number();
+    const validations = sut.field(field).required().min(length).email().build();
+
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, length),
+      new EmailValidation(field)
+    ]);
   });
 });
