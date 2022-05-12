6741946aa53cd1d3f048c0155a8bf37cf530f989
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-12 12:10:02 -0300

    refactor: lint code base

--- src/main/factories/pages/index.ts
+++ src/main/factories/pages/index.ts
@@ -1,3 +1,2 @@
 export * from './login/login-validation-factory';
 export * from './signup/signup-validation-factory';
-
--- src/presentation/test/index.ts
+++ src/presentation/test/index.ts
@@ -3,5 +3,3 @@ export * from './mock-add-account';
 export * from './mock-authentication';
 export * from './mock-save-access-token';
 export * from './mock-validation';
-
-
