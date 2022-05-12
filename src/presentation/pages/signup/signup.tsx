1864ee33cd72f9ef99786c47f36acc4cf655212b
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-12 02:37:30 -0300

    feat: ensure signup calls AddAccount with correct values

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,3 +1,4 @@
+import { AddAccount } from '@/domain/usecases';
 import {
   Footer,
   FormStatus,
@@ -11,9 +12,10 @@ import Styles from './sign-up-styles.scss';
 
 type Props = {
   validation: Validation;
+  addAccount: AddAccount;
 };
 
-const SignUp: React.FC<Props> = ({ validation }: Props) => {
+const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -45,6 +47,13 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
   ): Promise<void> => {
     event.preventDefault();
     setState({ ...state, isLoading: true });
+
+    await addAccount.add({
+      name: state.name,
+      email: state.email,
+      password: state.password,
+      passwordConfirmation: state.passwordConfirmation
+    });
   };
 
   return (
