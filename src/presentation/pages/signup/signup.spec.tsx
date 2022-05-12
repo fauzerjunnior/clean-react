2d06b4f60cc51d678aa4b3a8599995b9b35d2ca6
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-12 03:01:03 -0300

    feat: ensure signup prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -168,4 +168,12 @@ describe('SignUp component', () => {
 
     expect(addAccountSpy.callsCount).toBe(1);
   });
+
+  it('should not call AddAccount if form is invalid', async () => {
+    const validationError = faker.random.words();
+    const { sut, addAccountSpy } = makeSut({ validationError });
+
+    await simulateValidSubmit(sut);
+    expect(addAccountSpy.callsCount).toBe(0);
+  });
 });
