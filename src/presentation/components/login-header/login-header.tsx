478dcf4581159d6513db906a19f2ea096e9cead5
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-18 18:39:40 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -1,10 +1,10 @@
-import React, { memo } from 'react';
 import Logo from '@/presentation/components/logo/logo';
+import React, { memo } from 'react';
 import Styles from './login-header-styles.scss';
 
 const LoginHeader: React.FC = () => {
   return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
       <Logo />
       <h1>4DEV - Enquetes para programadores</h1>
     </header>
