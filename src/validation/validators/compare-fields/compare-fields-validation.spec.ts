965b691e96f5e3b07dfb6d22f9ce13e410791ef1
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-19 19:48:50 -0300

    fix: change CompareFields mock field values

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -8,27 +8,27 @@ const makeSut = (
 ): CompareFieldsValidation =>
   new CompareFieldsValidation(field, fieldToCompare);
 
-describe('ComparteFieldsValidation', () => {
-  test('should return error if compare is invalid', () => {
+describe('CompareFieldsValidation', () => {
+  test('Should return error if compare is invalid', () => {
     const field = faker.database.column();
     const fieldToCompare = faker.database.column();
-
     const sut = makeSut(field, fieldToCompare);
     const error = sut.validate({
-      [field]: faker.random.words(3),
+      [field]: faker.random.words(1),
       [fieldToCompare]: faker.random.words(4)
     });
-
     expect(error).toEqual(new InvalidFieldError());
   });
 
-  test('should return falsy if compare is valid', () => {
+  test('Should return falsy if compare is valid', () => {
     const field = faker.database.column();
     const fieldToCompare = faker.database.column();
-
+    const value = faker.random.word();
     const sut = makeSut(field, fieldToCompare);
-    const error = sut.validate({ field, fieldToCompare });
-
+    const error = sut.validate({
+      [field]: value,
+      [fieldToCompare]: value
+    });
     expect(error).toBeFalsy();
   });
 });
