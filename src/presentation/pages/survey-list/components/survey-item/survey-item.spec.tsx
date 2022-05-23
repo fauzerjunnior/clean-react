ba208c5cc35ae82863d7e4fbf0329975a275c7a8
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-23 15:31:58 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -23,4 +23,22 @@ describe('SurveyItem', () => {
     expect(screen.getByTestId('month')).toHaveTextContent('jul');
     expect(screen.getByTestId('year')).toHaveTextContent('2022');
   });
+
+  test('should with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2018-03-03T00:00:00')
+    });
+
+    makeSut(survey);
+
+    expect(screen.getByTestId('icon')).toHaveProperty(
+      'src',
+      IconName.thumbDown
+    );
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('03');
+    expect(screen.getByTestId('month')).toHaveTextContent('mar');
+    expect(screen.getByTestId('year')).toHaveTextContent('2018');
+  });
 });
