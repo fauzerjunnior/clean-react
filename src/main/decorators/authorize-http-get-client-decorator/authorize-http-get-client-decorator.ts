31fd46e7ef53b00cedd3c96815e6d46b98a38364
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-24 19:11:28 -0300

    feat: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -17,9 +17,9 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
 
     if (account?.accessToken) {
       Object.assign(params, {
-        headers: {
+        headers: Object.assign(params.headers || {}, {
           'x-access-token': account.accessToken
-        }
+        })
       });
     }
 
