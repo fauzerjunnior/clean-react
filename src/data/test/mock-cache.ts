20b8cf2b8dcac26f42e6c82a8eadfafcbe2ce796
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-24 14:36:01 -0300

    test: ensure AuthorizeHttpGetClientDecorator calls GetStorage with correct value

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -1,4 +1,5 @@
-import { SetStorage } from '@/data/protocols/cache';
+import { SetStorage, GetStorage } from '@/data/protocols/cache';
+import faker from '@faker-js/faker';
 
 export class SetStorageMock implements SetStorage {
   key: string;
@@ -9,5 +10,17 @@ export class SetStorageMock implements SetStorage {
   set(key: string, value: any): void {
     this.key = key;
     this.value = value;
+  }
+}
+
+export class GetStorageSpy implements GetStorage {
+  key: string;
+
+  value = faker.datatype.json();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  get(key: string): any {
+    this.key = key;
+    return this.value;
   }
 }
--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -1,11 +1,15 @@
 import { GetStorage } from '@/data/protocols/cache';
-import { HttpGetClient, HttpResponse } from '@/data/protocols/http';
+import {
+  HttpGetClient,
+  HttpGetParams,
+  HttpResponse
+} from '@/data/protocols/http';
 
 export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   constructor(private readonly getStorage: GetStorage) {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async get(params: HttpGetClient): Promise<HttpResponse> {
+  async get(params: HttpGetParams): Promise<HttpResponse> {
     this.getStorage.get('account');
     return null;
   }
