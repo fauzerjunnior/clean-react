d6e480d90758644cd90a357129dd0609b91d533e
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-25 23:51:07 -0300

    test: ensure SurveyList render error on failure

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -5,6 +5,7 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { LoadSurveyList } from '@/domain/usecases';
 import { mockSurveyListModel } from '@/domain/test';
+import { UnexpectedError } from '@/domain/errors';
 
 const history = createMemoryHistory({ initialEntries: ['/'] });
 
@@ -24,8 +25,7 @@ type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy;
 };
 
-const makeSut = (): SutTypes => {
-  const loadSurveyListSpy = new LoadSurveyListSpy();
+const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
   render(
     <Router navigator={history} location={history.location}>
       <SurveyList loadSurveyList={loadSurveyListSpy} />
@@ -40,6 +40,7 @@ describe('SurveyList Component', () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
     await waitFor(() => surveyList);
   });
 
@@ -54,5 +55,18 @@ describe('SurveyList Component', () => {
     const surveyList = screen.getByTestId('survey-list');
     await waitFor(() => surveyList);
     expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3);
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+  });
+
+  test('should render error on failure', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(error);
+
+    makeSut(loadSurveyListSpy);
+
+    await waitFor(() => screen.getByRole('heading'));
+    expect(screen.queryByTestId('survey-list')).not.toBeInTheDocument();
+    expect(screen.getByTestId('error')).toHaveTextContent(error.message);
   });
 });
