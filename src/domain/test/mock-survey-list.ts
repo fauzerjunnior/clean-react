18797d132047723f13264fdf10673b4b27fc8372
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-05-29 23:28:35 -0300

    refactor: move LoadSurveyListSpy to a factory method

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -13,3 +13,14 @@ export const mockSurveyListModel = (): LoadSurveyList.Model[] => [
   mockSurveyModel(),
   mockSurveyModel()
 ];
+
+export class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+
+  surveys = mockSurveyListModel();
+
+  async loadAll(): Promise<LoadSurveyList.Model[]> {
+    this.callsCount += 1;
+    return this.surveys;
+  }
+}
