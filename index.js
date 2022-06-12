807aa0d23ff57a1dd6f322c6024feb1db7a0b8f7
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-06-12 12:19:36 -0300

    feat: add csurf & helmet to improve express vulnerabilities

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fallback = require('express-history-api-fallback');
 const express = require('express');
+const csrf = require('csurf');
+const helmet = require('helmet');
 
 const app = express();
 const root = `${__dirname}/dist`;
 app.use(express.static(root));
+app.use(csrf({ cookie: true }));
+app.use(helmet.hidePoweredBy());
 app.use(fallback('index.html', { root }));
 app.listen(process.env.PORT || 3000);
