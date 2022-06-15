10b680dcf9e7d3ebf49fd8ce4f51fcc2960822b9
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-06-14 22:38:48 -0300

    feat: add SurveyResult layout

---
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -0,0 +1,52 @@
+import { Footer, Header, Spinner } from '@/presentation/components';
+import React from 'react';
+import Styles from './survey-result-styles.scss';
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Qual é seu framework web favorito?</h2>
+        <ul className={Styles.answersList}>
+          <li>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+              alt="React"
+            />
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-vue.png"
+              alt="Vue"
+            />
+            <span className={Styles.answer}>VueJS</span>
+            <span className={Styles.percent}>30%</span>
+          </li>
+          <li>
+            <img
+              src="http://fordevs.herokuapp.com/static/img/logo-angular.png"
+              alt="React"
+            />
+            <span className={Styles.answer}>Angular</span>
+            <span className={Styles.percent}>20%</span>
+          </li>
+        </ul>
+
+        <button type="button">Voltar</button>
+
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde...</span>
+            <Spinner isNegative />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default SurveyResult;
