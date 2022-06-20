9557422f25f6e54534f81e888c2dea4220d90da4
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-06-20 13:55:47 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -7,38 +7,42 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar className={Styles.calendarWrap} date={new Date()} />
-          <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <ul className={Styles.answersList}>
-          <li>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-react.png"
-              alt="React"
-            />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-vue.png"
-              alt="Vue"
-            />
-            <span className={Styles.answer}>VueJS</span>
-            <span className={Styles.percent}>30%</span>
-          </li>
-          <li>
-            <img
-              src="http://fordevs.herokuapp.com/static/img/logo-angular.png"
-              alt="React"
-            />
-            <span className={Styles.answer}>Angular</span>
-            <span className={Styles.percent}>20%</span>
-          </li>
-        </ul>
+        {true && (
+          <>
+            <hgroup>
+              <Calendar className={Styles.calendarWrap} date={new Date()} />
+              <h2>Qual é seu framework web favorito?</h2>
+            </hgroup>
+            <ul className={Styles.answersList}>
+              <li>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-react.png"
+                  alt="React"
+                />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-vue.png"
+                  alt="Vue"
+                />
+                <span className={Styles.answer}>VueJS</span>
+                <span className={Styles.percent}>30%</span>
+              </li>
+              <li>
+                <img
+                  src="http://fordevs.herokuapp.com/static/img/logo-angular.png"
+                  alt="React"
+                />
+                <span className={Styles.answer}>Angular</span>
+                <span className={Styles.percent}>20%</span>
+              </li>
+            </ul>
 
-        <button type="button">Voltar</button>
+            <button type="button">Voltar</button>
+          </>
+        )}
 
         {false && <Loading />}
       </div>
