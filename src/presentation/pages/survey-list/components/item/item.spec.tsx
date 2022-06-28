a1f4208ac3f8e6f5c0b89ee3d0c5f3da58f40c07
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-06-28 09:04:59 -0300

    fix: move Calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -11,18 +11,24 @@ const makeSut = (survey = mockSurveyModel()): void => {
 describe('SurveyItem', () => {
   test('should with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2022-07-10T00:00:00')
     });
 
     makeSut(survey);
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+
+    expect(screen.getByTestId('day')).toHaveTextContent('10');
+    expect(screen.getByTestId('month')).toHaveTextContent('jul');
+    expect(screen.getByTestId('year')).toHaveTextContent('2022');
   });
 
   test('should with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2018-03-03T00:00:00')
     });
 
     makeSut(survey);
@@ -32,5 +38,8 @@ describe('SurveyItem', () => {
       IconName.thumbDown
     );
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('03');
+    expect(screen.getByTestId('month')).toHaveTextContent('mar');
+    expect(screen.getByTestId('year')).toHaveTextContent('2018');
   });
 });
