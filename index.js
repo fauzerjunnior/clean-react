351e0fb8d0073bca353290bb225c9e2a08505462
Fauzer Junior <fauzerjunnior@hotmail.com>
2022-06-28 14:07:03 -0300

    feat: add cookie parser on server

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const fallback = require('express-history-api-fallback');
 const express = require('express');
 const csrf = require('csurf');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const root = `${__dirname}/dist`;
 app.use(express.static(root));
+app.use(cookieParser());
 app.use(csrf({ cookie: true }));
 app.use(helmet.hidePoweredBy());
 app.use(fallback('index.html', { root }));
